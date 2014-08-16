92928555124b6a459f73c041c3cfaa5245b22e57
Andrew Butler <abutler3@gmail.com>
2014-08-15 19:33:05 -0500

    Adding product and sale model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,13 +25,14 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
 
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.json { render json: @product, status: :created, location: @product }
       else
-        format.html { render :new }
+        format.html { render 'new' }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
@@ -69,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :permalink, :description, :price, :user_id)
+      params.require(:product).permit(:name, :permalink, :description, :price, :file)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :sales
+
   resources :products
 
   devise_for :users
