e5862f74de35135cee9db10e707b3be6ef896d8c
Andrew Butler <abutler3@gmail.com>
2014-08-18 21:00:06 -0500

    Add validator to image

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,7 @@ class Product < ActiveRecord::Base
 
 
   validates_numericality_of :price, greater_than: 0, message: "must be at least 50 cents"
+
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
